c2f6dcfb326dcf3385f4eaebd8c65a958b6201f0
wangyongchuan01 <wangyongchuan01@58ganji.com>
2016-08-12 21:05:39 +0800

    首页代码更新

--- app/src/main/java/com/seventeam/wubahuichang/Activity/LaunchActivity.java
+++ app/src/main/java/com/seventeam/wubahuichang/Activity/LaunchActivity.java
@@ -1,15 +1,31 @@
 package com.seventeam.wubahuichang.Activity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.seventeam.wubahuichang.R;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends FragmentActivity {
 
+    private Handler handler=new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(LaunchActivity.this,MainActivity.class);
+                LaunchActivity.this.startActivity(intent);
+                LaunchActivity.this.finish();
+
+            }
+        },3000);
+
     }
 }
